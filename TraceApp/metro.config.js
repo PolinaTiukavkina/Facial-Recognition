16d92e17610fa08f7d680e6414099d9b11930f15
Polina <150296398+PolinaTiukavkina@users.noreply.github.com>
2025-09-24 01:00:39 +0100

    Delete TraceApp directory

--- TraceApp/metro.config.js
+++
@@ -1,9 +0,0 @@
-// metro.config.js
-const { getDefaultConfig } = require("expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-
-// Ensure images like .png, .jpg are supported
-config.transformer.assetPlugins = ["expo-asset/tools/hashAssetFiles"];
-
-module.exports = config;
